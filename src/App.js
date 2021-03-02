f3f24962ede72852540c2e06c5b82eedd7b62027
Sean MacEachern <74990225+sean-mac1357@users.noreply.github.com>
2021-03-02 12:02:58 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import LoginForm from "./components/LoginForm";
+import "./styles.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      credentials: {
+        seanmac: {
+          password: "123qwaszx"
+        }
+      }
+    };
+  }
+
+  _checkCredentials = (username, password) => {
+    console.log("checking credentials");
+    const userObj = this.state.credentials[username];
+    if (userObj && userObj.password === password) {
+      return {
+        isValid: true,
+        message: "Login successful"
+      };
+    } else {
+      return {
+        isValid: false,
+        message: "Bad username or password"
+      };
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <LoginForm handleSubmit={this._checkCredentials} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import LoginMessage from "./LoginMessage";
+
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      serverResponse: null
+    };
+  }
+
+  _onChange = (field, value) => {
+    this.setState({
+      [field]: value
+    });
+  };
+
+  _handleSubmit = (event) => {
+    event.preventDefault();
+    const serverResponse = this.props.handleSubmit(
+      this.state.username,
+      this.state.password
+    );
+    this.setState(
+      {
+        serverResponse
+      },
+      () => {
+        console.log("server response is: ", serverResponse);
+      }
+    );
+  };
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this._handleSubmit}>
+          <label>
+            Username:
+            <input
+              type="text"
+              name="username"
+              placeholder="Your Username"
+              value={this.state.username}
+              onChange={(event) => {
+                this._onChange("username", event.target.value);
+              }}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              name="password"
+              placeholder="Your Password"
+              value={this.state.password}
+              onChange={(event) => {
+                this._onChange("password", event.target.value);
+              }}
+            />
+          </label>
+          <button type="submit">Login</button>
+        </form>
+        {!!this.state.serverResponse ? (
+          <LoginMessage {...this.state.serverResponse} />
+        ) : null}
+      </>
+    );
+  }
+}
+
+export default LoginForm;
